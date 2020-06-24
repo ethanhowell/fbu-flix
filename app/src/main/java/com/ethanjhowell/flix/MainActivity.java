0ba40eb9bd80c8057f846e6fa959e73365d4eb00
Ethan J. Howell <21162595+ethanhowell@users.noreply.github.com>
2020-06-24 15:39:45 -0500

    Created app simply showing list of movies.
    * created empty project
    
    * added internet permission and async client dependency
    
    * got the json downloading for movies now playing
    
    * got movie model created and storing the list of movies in there as well
    
    * created xml for displaying the movie list
    
    * got movies to show up on the phone
    
    * ui makes more sense now. apps don't take up whole screen
    
    Co-authored-by: Ethan Howell <ethanhowell@fb.com>

---
+++ app/src/main/java/com/ethanjhowell/flix/MainActivity.java
@@ -0,0 +1,65 @@
+package com.ethanjhowell.flix;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.ethanjhowell.flix.adapters.MovieAdapter;
+import com.ethanjhowell.flix.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    private final String TAG = getClass().getCanonicalName();
+
+    List<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        movies = new ArrayList<>();
+
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        final MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+        rvMovies.setAdapter(movieAdapter);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "onSuccess: Results " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.d(TAG, "onSuccess: Num movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "onSuccess: Failure getting results from response", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+    }
+}
