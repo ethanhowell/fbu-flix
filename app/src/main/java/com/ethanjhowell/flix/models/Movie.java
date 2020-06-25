71a3754e68ca10cc466d4cb7e49e0014f6009014
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-06-25 10:10:01 -0500

    got the landscape working

--- app/src/main/java/com/ethanjhowell/flix/models/Movie.java
+++ app/src/main/java/com/ethanjhowell/flix/models/Movie.java
@@ -29,11 +29,11 @@ public class Movie {
     }
 
     public String getPosterPath() {
-        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+        return String.format("https://image.tmdb.org/t/p/w342%s", posterPath);
     }
 
     public String getBackdropPath() {
-        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+        return String.format("https://image.tmdb.org/t/p/w342%s", backdropPath);
     }
 
     public String getTitle() {
