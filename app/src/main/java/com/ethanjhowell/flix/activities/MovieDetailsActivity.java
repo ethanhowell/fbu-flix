2b841d3837c99c09f73df9f892ea6e30984facbc
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-06-25 15:15:05 -0500

    intent passing works!

--- app/src/main/java/com/ethanjhowell/flix/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/ethanjhowell/flix/activities/MovieDetailsActivity.java
@@ -1,16 +1,26 @@
 package com.ethanjhowell.flix.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ethanjhowell.flix.R;
+import com.ethanjhowell.flix.models.Movie;
+
+import org.parceler.Parcels;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+    private final static String TAG = MovieDetailsActivity.class.getCanonicalName();
+    Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        // unwrap the movie from the parcel
+        movie = Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d(TAG, String.format("Showing details for \"%s\"", movie.getTitle()));
     }
 }
